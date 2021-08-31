11396de069132425a510543fd796e177261fc30e
TranNhutThong1999 <thongmap0909310872@gmail.com>
2021-08-31 09:39:32 +0700

    ok

---
+++ src/main/java/kltn/Application.java
@@ -0,0 +1,29 @@
+ package kltn;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.github.messenger4j.Messenger;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableAsync
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class Application  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+    @Bean
+    public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
+ @Value("${messenger4j.appSecret}") final String appSecret,
+ @Value("${messenger4j.verifyToken}") final String verifyToken) {
+        return Messenger.create(pageAccessToken, appSecret, verifyToken);
+    }
+}
---
+++ src/main/java/kltn/controller/HomeController.java
@@ -0,0 +1,100 @@
+package kltn.controller;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.messenger4j.Messenger;
+import com.github.messenger4j.exception.MessengerApiException;
+import com.github.messenger4j.exception.MessengerIOException;
+import com.github.messenger4j.exception.MessengerVerificationException;
+import com.github.messenger4j.send.MessagePayload;
+import com.github.messenger4j.send.MessagingType;
+import com.github.messenger4j.send.NotificationType;
+import com.github.messenger4j.send.message.TextMessage;
+import com.github.messenger4j.send.recipient.IdRecipient;
+import com.github.messenger4j.webhook.event.TextMessageEvent;
+
+import static com.github.messenger4j.Messenger.CHALLENGE_REQUEST_PARAM_NAME;
+import static com.github.messenger4j.Messenger.MODE_REQUEST_PARAM_NAME;
+import static com.github.messenger4j.Messenger.SIGNATURE_HEADER_NAME;
+import static com.github.messenger4j.Messenger.VERIFY_TOKEN_REQUEST_PARAM_NAME;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/webhook")
+public class HomeController {
+
+	    private final Messenger messenger;
+
+	    @Autowired
+	    public HomeController(final Messenger messenger) {
+	        this.messenger = messenger;
+	    }
+
+	    @GetMapping
+	    public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
+	                                                @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken, @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
+	        try {
+	            this.messenger.verifyWebhook(mode, verifyToken);
+	            return ResponseEntity.ok(challenge);
+	        } catch (MessengerVerificationException e) {
+	            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+	        }
+	    }
+
+	    @PostMapping
+	    public ResponseEntity<Void> handleCallback(@RequestBody final String payload, @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) throws MessengerVerificationException {
+	        this.messenger.onReceiveEvents(payload, of(signature), event -> {
+	            if (event.isTextMessageEvent()) {
+	                try {
+	                    handleTextMessageEvent(event.asTextMessageEvent());
+	                } catch (MessengerApiException e) {
+	                    e.printStackTrace();
+	                } catch (MessengerIOException e) {
+	                    e.printStackTrace();
+	                }
+	            } else {
+	                String senderId = event.senderId();
+	                sendTextMessageUser(senderId, "Tôi là bot chỉ có thể xử lý tin nhắn văn bản.");
+	            }
+	        });
+	        return ResponseEntity.status(HttpStatus.OK).build();
+	    }
+
+	    private void handleTextMessageEvent(TextMessageEvent event) throws MessengerApiException, MessengerIOException {
+	        final String senderId = event.senderId();
+	        sendTextMessageUser(senderId, "Xin chào! Đây là chatbot được tạo từ ứng dụng Spring Boot");
+
+	    }
+
+	    private void sendTextMessageUser(String idSender, String text) {
+	        try {
+	            final IdRecipient recipient = IdRecipient.create(idSender);
+	            final NotificationType notificationType = NotificationType.REGULAR;
+	            final String metadata = "DEVELOPER_DEFINED_METADATA";
+
+	            final TextMessage textMessage = TextMessage.create(text, empty(), of(metadata));
+	            final MessagePayload messagePayload = MessagePayload.create(recipient, MessagingType.RESPONSE, textMessage,
+	                    of(notificationType), empty());
+	            this.messenger.send(messagePayload);
+	        } catch (MessengerApiException | MessengerIOException e) {
+	            handleSendException(e);
+	        }
+	    }
+
+	    private void handleSendException(Exception e) {
+	    }
+}
